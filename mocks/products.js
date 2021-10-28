ad13ee74f8bc4e6dedbcdb6f7c114c1f1a3177ec
Алексей Сергеев <pendelladam@gmail.com>
2021-10-28 13:51:47 +0300

    uploaded mock database to the Mongo Atlas

--- mocks/products.js
+++ mocks/products.js
@@ -2,8 +2,56 @@ const utils = require("../utils");
 
 // mongoDB connection
 const mongoose = require("mongoose");
-const MONGO_DB_URI = require("../const").MONGO_DB_URI;
-mongoose.connect(MONGO_DB_URI);
+/* const MONGO_DB_URI = require("../const").MONGO_DB_URI;
+mongoose.connect(MONGO_DB_URI); */
+
+const MONGO_ATLAS_URI =
+  "mongodb+srv://daddy:oiGbw2h1YlBLmb52@cluster0.l5vm3.mongodb.net/technomart?retryWrites=true&w=majority";
+
+mongoose
+  .connect(MONGO_ATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected...");
+
+    TYPES.forEach(async (type) => {
+      const productType = new Product({ name: type });
+      const products = generateProducts(PRODUCT_COUNT, type);
+      const vendors = new Set();
+
+      products.forEach((product) => {
+        const model = require(`../models/product-item/discriminators/${product.type}`);
+
+        const newProduct = new model({
+          vendor: product.vendor,
+          model: product.model,
+          name: product.name,
+          image: product.image,
+          isBatteryOnly: product.isBatteryOnly,
+          isNewOffer: product.isNewOffer,
+          price: product.price,
+          length: product.length,
+          fuelCapacity: product.fuelCapacity,
+          isCordless: product.isCordless,
+        });
+
+        productType.catalog.push(newProduct);
+        vendors.add(product.vendor);
+      });
+
+      productType.vendors = [...vendors];
+      productType.count = productType.catalog.length;
+
+      await productType
+      .save()
+      .then(() => console.log("product added"))
+      .catch((err) => console.log(err))
+      .finally(() => mongoose.disconnect());
+    });
+  })
+  .catch((err) => console.log(err));
 
 const Product = require("../models/product/product");
 
@@ -45,33 +93,3 @@ function generateProducts(count, type) {
     return product;
   });
 }
-
-TYPES.forEach(async (type) => {
-  const productType = new Product({ name: type });
-  const products = generateProducts(PRODUCT_COUNT, type);
-  const vendors = new Set();
-
-  products.forEach(async (product) => {
-    const model = require(`../models/product-item/discriminators/${product.type}`);
-
-    const newProduct = new model({
-      vendor: product.vendor,
-      model: product.model,
-      name: product.name,
-      image: product.image,
-      isBatteryOnly: product.isBatteryOnly,
-      isNewOffer: product.isNewOffer,
-      price: product.price,
-      length: product.length,
-      fuelCapacity: product.fuelCapacity,
-      isCordless: product.isCordless,
-    });
-
-    productType.catalog.push(newProduct);
-    vendors.add(product.vendor);
-  });
-
-  productType.vendors = [...vendors];
-  productType.count = productType.catalog.length;
-  await productType.save();
-});
