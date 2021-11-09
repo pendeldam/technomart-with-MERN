52b0c34deba44768579f80d483e26fe42f8d0ba9
Алексей Сергеев <pendelladam@gmail.com>
2021-11-09 13:49:45 +0300

    added client app routes; removed client mocks;

--- client/src/components/app/app.jsx
+++ client/src/components/app/app.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "../main-page/main-page";
 import CatalogPage from "../catalog-page/catalog-page";
+import ProductList from "../product-list/product-list";
+import ProductPage from "../product-page/product-page";
 import { AppRoute } from "../../const";
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path={AppRoute.ROOT} element={<MainPage />} />
         <Route path={AppRoute.CATALOG} element={<CatalogPage />} />
+        <Route path={`${AppRoute.CATALOG}/:type`} element={<ProductList />} />
+        <Route path={`${AppRoute.CATALOG}/:type/:id`} element={<ProductPage />} />
       </Routes>
     </BrowserRouter>
   );
