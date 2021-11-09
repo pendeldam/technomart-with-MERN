ba32dd1c2de95c5e6f931f8d06c7b9fdfeb80ce0
Алексей Сергеев <pendelladam@gmail.com>
2021-11-09 21:22:58 +0300

    added sorting direction;

--- client/src/components/catalog-sorting/catalog-sorting.jsx
+++ client/src/components/catalog-sorting/catalog-sorting.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getSortType, getSortDirection } from "../../store/selectors/appData";
 import { setSortType, setSortDirection } from "../../store/actions";
-import { SortTypes, SortLabels } from "../../const";
+import { SortTypes, SortLabels, SortDirection } from "../../const";
 
 function CatalogSorting() {
   const dispatch = useDispatch();
   const sortType = useSelector(getSortType);
+  const sortDirection = useSelector(getSortDirection);
 
   return (
     <section className="catalog-sorting">
@@ -20,48 +21,38 @@ function CatalogSorting() {
                 type="radio"
                 name="sort-type"
                 id={`sort-by-${key}`}
+                checked={key.toUpperCase() === sortType}
                 onChange={() =>
                   dispatch(setSortType(SortTypes[key.toUpperCase()]))
                 }
-                checked={key.toUpperCase() === sortType}
               />
               <label htmlFor={`sort-by-${key}`}>{value}</label>
             </div>
           ))}
         </div>
         <div className="catalog-sorting__direction">
-          <input
-            className="visually-hidden"
-            id="sort-direction-up"
-            type="radio"
-            name="sort-direction"
-          />
-          <label htmlFor="sort-direction-up">
-            <svg
-              width="11"
-              height="10"
-              viewBox="0 0 11 10"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M5.5 0L11 10L0 10" />
-            </svg>
-          </label>
-          <input
-            className="visually-hidden"
-            id="sort-direction-down"
-            type="radio"
-            name="sort-direction"
-          />
-          <label htmlFor="sort-direction-down">
-            <svg
-              width="11"
-              height="10"
-              viewBox="0 0 11 10"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M5.5 10L0 0H11" />
-            </svg>
-          </label>
+          {Object.values(SortDirection).map((value) => (
+            <div className="catalog-sorting__direction-toggle">
+              <input
+                className="visually-hidden"
+                id={`sort-direction-${value.toLocaleLowerCase()}`}
+                type="radio"
+                name="sort-direction"
+                checked={value === sortDirection}
+                onChange={() => dispatch(setSortDirection(value))}
+              />
+              <label htmlFor={`sort-direction-${value.toLocaleLowerCase()}`}>
+                <svg
+                  width="11"
+                  height="10"
+                  viewBox="0 0 11 10"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M5.5 0L11 10L0 10" />
+                </svg>
+              </label>
+            </div>
+          ))}
         </div>
       </form>
     </section>
